ae42b27f2e6a5289dbda5362632085bc29024269
sarenne <sarenne.wallbridge@gmail.com>
2021-02-01 17:16:22 +0000

    :tada: First working version of testmaker for Discriminative Turns testmaker_discrim.py is completely based on the more general testmaker.py script; it pulls from its own template file, and uses similar 'resources' to read urls.
    I didn't add this as a template because each experiment 'question' is
    actually a set of multiple qualtrics questions so woudl require more
    refactoring. Questions are build from the experiment_player notenbook in
    `Discriminative_Turns` repo rather than pulling text from txt files.

---
+++ discriminative_turn_utils.py
@@ -0,0 +1,125 @@
+import glob
+import json
+import random
+import re
+
+""""""
+
+def print_transcript(text):
+    """
+    Clean transcript for printing in the experiment player:
+    - unintelligible speech: keep
+    - speech events (eg. {laugh}, [distortion]): keep
+    - punctuation (eg \, \. \- _should this include '?'_): keep
+    """
+    # remove filler speech, and capitalisation symbols
+    clean = re.sub(r'[%^~]', '', text)
+#     # remove unintelligible speech
+#     clean = re.sub(r'\(.*\)', '', clean)
+#     # remove speech acts
+#     clean = re.sub(r"[\{\[].*?[\}\]]", "", clean)
+#     # remove punctuation
+#     clean = re.sub(r"[\,\.\?]", "", clean)
+    # remove trailing and multiple spaces
+    clean = re.sub(' +', ' ', clean).strip()
+    return clean
+
+def context_print(utter_id, conversation, turns_before, turns_after, individual=False):
+    """Print the speaker turns before and after the target utterance (utter1)"""
+
+    # # Load conversation (list of utterance dict objs)
+    # with open(experiment_path + '/conversation.json') as json_file:
+    #     c = json.load(json_file)
+    c = conversation
+
+    # Split conversation into speaker turns (list of utterances grouped by speaker)
+    speaker_list = []
+    speaker = c[0]['speaker']
+    cont = []
+    for utter in c:
+        if speaker == utter['speaker']:
+            cont.append(utter)
+        else:
+            speaker_list.append(cont)
+            cont = [utter]
+            speaker = utter['speaker']
+    speaker_list.append(cont)
+
+    # Convert list of turns into single turn object
+    speaker_turns = []
+    for i, elem in enumerate(speaker_list):
+        turn = {k: [d[k] for d in elem] for k in elem[0]}
+        turn['turn_id'] = i
+        speaker_turns.append(turn)
+
+    # Find the target utter as a target turn
+    target_turn = [turn for turn in speaker_turns if utter_id in turn['utter_id']][0]
+    target_turn_id = target_turn['turn_id']
+
+    # Print turns
+    def print_turn(turn, utter1_all, individual):
+        """
+        Print each utterance in the speaker turn. Special behaviour for utter1 turn: only display utter1
+        unless context AFTER is >0, then display all potential utterances in the turn (utter1_all).
+        """
+        turn_str = ""
+        speaker = turn["speaker"][0]
+
+        # BOLD utter0 (full turn)
+        if utter_id - 1 in turn["utter_id"]:
+            for text in turn["text"]:
+                if individual:
+                    turn_str += f'{side_starts[speaker]} {print_transcript(text)} {end}'
+                else:
+                    turn_str += f'{side_starts[speaker]} <b>{print_transcript(text)}</b> {end}'
+        # BOLD utter1
+        elif utter_id in turn["utter_id"]:
+            turn_str += f'{side_starts[speaker]} <b>{print_transcript(turn["text"][0])}</b> {end}'
+            if utter1_all and len(turn['text']):
+                for text in turn["text"][1:]:
+                    turn_str += f'{side_starts[speaker]} {print_transcript(text)} {end}'
+        # normal other utters (full turns)
+        else:
+            for text in turn["text"]:
+                turn_str += f'{side_starts[speaker]} {print_transcript(text)} {end}'
+
+        return turn_str
+
+    # Formatting
+    def markdown_start(align, color, font_size=12, width=500):
+        """Return the markdown start symbol for a particular speaker (alignment, color)"""
+        return f'<div style="text-align: {align}; width:{width}px"> <span style="color:{color}; font-size: {font_size}pt">'
+
+    side_starts = {"A": markdown_start('right', '#065B9A'), #'<div style="text-align: right"> <span style="color:#065B9A; font-size: 18pt">',
+                   "B": markdown_start('left', '#069A62'),}
+    end = "</span> </div>"
+
+    # The actual printing...
+    full_string = ""
+    for turn in speaker_turns[max(0, target_turn_id - turns_before - 1 ):
+                               min(target_turn_id + turns_after + 1, len(speaker_turns))]:
+        full_string += print_turn(turn, turns_after>0, individual)
+
+    return full_string
+
+def get_urls(experiment_ids=[], write=False,
+             base_path='/group/project/cstr3/html/sarenne/test_qualtrics/',
+             base_url='https://groups.inf.ed.ac.uk/cstr3/sarenne/test_qualtrics/'):
+    """
+    Return the urls for files stored in https://groups.inf.ed.ac.uk/cstr3/ by checking nfs
+
+    NOTE access to nfs requires working through SSH! Therefore, use scp to copy back to local machine.
+    """
+    nfs_path = '/afs/inf.ed.ac.uk/user/s13/s1301730/Documents/discriminitive_turns/qualtrics_resources'
+    # http://data.cstr.ed.ac.uk/sarenne/test_qualtrics/
+    # https://groups.inf.ed.ac.uk/cstr3/sarenne/test_qualtrics/sw_40106_148_4_4_2/
+    if len(experiment_ids) < 1:
+        experiment_ids = [p.split('/')[-1] for p in glob.glob(base_path + '/*')]
+
+    experiment_data = {exp_id: glob.glob(base_path + exp_id + '/*.wav') for exp_id in experiment_ids}
+    url_data = {exp_id: [base_url + exp_id + '/' + e.split('/')[-1] for e in files] for exp_id, files in experiment_data.items()}
+    if write:
+        with open(nfs_path + '/urls.json', 'w+') as fs:
+            json.dump(url_data, fs)
+
+    return experiment_data, url_data
---
+++ testmaker_discrim.py
@@ -0,0 +1,246 @@
+
+# import argparse
+import copy
+import glob
+import json
+import requests
+
+from string import Template
+from collections import OrderedDict
+
+import config
+
+from discriminative_turn_utils import *
+
+""""""
+
+##### Define global params #####
+# Input and output json objects for qualtrics survey
+JSON_TEMPLATE = "template_discrim.json"
+SAVE_TEMPLATE = "output-survey_discrim.qsf"
+
+# audio templates should not be changed
+AUDIO_HTML_TEMPLATE = "audio_template.html"
+PLAY_BUTTON = "play_button.html"
+
+# Where data is stored
+BASE_PATH ='/group/project/cstr3/html/sarenne/test_qualtrics/' # where generated experiment datas are stored
+BASE_URL = 'https://groups.inf.ed.ac.uk/cstr3/sarenne/test_qualtrics/'
+URLS_PATH = 'discrim_turn_resources/urls.json'
+
+##### Define global methods #####
+# load JSON template from file
+def get_basis_json():
+    with open(JSON_TEMPLATE) as json_file:
+        return json.load(json_file)
+
+# standard audio player for all question types except MUSHRA
+def get_player_html(url):
+    with open(AUDIO_HTML_TEMPLATE) as html_file:
+        return Template(html_file.read()).substitute(url=url)
+
+def path_to_url(path):
+    """Convert path to group.inf.ed.ac.uk data to url"""
+    pass
+
+# audio player with only play/pause controls for MUSHRA tests
+# to prevent participants identifying hidden reference by duration
+def get_play_button(url, n): # player n associates play button with a specific audio
+    with open(PLAY_BUTTON) as html_file:
+        return Template(html_file.read()).substitute(url=url, player=n)
+
+def make_discrim_question_set(q_counter, experiment_id, audio_urls, contexts, basis_question):
+    """
+
+    Args
+        q_counter (int): to track the question ids for qualtrics "QuestionID" attribute (used as the ID of the first
+            question round in discrim set)
+        experiment_id (str): the dir_name of the experiment (identifier of conv_id, utter_d, seeds, correct_indx)
+        audio_urls (list): list of str urls
+        contexts ():
+        basis_question ():
+    """
+
+    def q_set_up(q_id, export_id):
+        """Init a template and fill with generic attributes (survey ID) and basic question attributes (q ID)"""
+        # Get template
+        new_q = copy.deepcopy(basis_question)
+        # Update the survey ID
+        new_q['SurveyID'] = config.survey_id
+        # Update question ID and related fields
+        new_q['Payload'].update({'QuestionID' : f'QID{q_id}',
+                                 'DataExportTag' : f'QID{export_id}',
+                                 'QuestionDescription' : f'Q{q_id}',
+                                 })
+        new_q.update({'PrimaryAttribute' : f'QID{q_id}',
+                      'SecondaryAttribute' : f'QID{q_id}'})
+        return new_q
+
+    def update_choices(q_json, audio_urls, individual=False):
+        """Fill matrix with audio samples for a particular experiment. Return updated question template object"""
+
+        indv_urls = [f for f in audio_urls if len(f.split("/")[-1][:-4])>2]
+        if individual:
+            urls = indv_urls
+        else:
+            urls = list(set(audio_urls) - set(indv_urls))
+
+        choice_template = q_json['Payload']['Choices']['1']# make choice template
+        # empty 'Choices' so flexible number can be added using Choice template
+        q_json['Payload']['Choices'] = {}
+        for i, audio in enumerate(urls):
+            choice = copy.deepcopy(choice_template)
+            choice['Display'] = get_player_html(audio) # add audio player as choice
+            q_json['Payload']['Choices'][f'{i+1}'] = choice
+        return q_json
+
+    def update_text(q_json, exp_id, turns_before, turns_after, individual=False):
+        """Fill in question text with the conversation context. Return updated question template object"""
+
+        conversation = json.loads(requests.get(BASE_URL + exp_id + '/conversation.json').text)
+        utter_id = int(exp_id.split("_")[-4])
+
+        q_text = context_print(utter_id, conversation, turns_before, turns_after, individual=individual)
+        q_json['Payload'].update({'QuestionText': q_text})
+        return q_json
+
+    # Store the question set  (NOTE should this be a list?)
+    q_set = []
+    q_exports = []
+
+    # Get individual context round & append
+    q_export = experiment_id + f'_Q0'
+    indiv = True
+    new_q = q_set_up(q_counter, q_export)
+    new_q = update_choices(new_q, audio_urls, individual=indiv)
+    new_q = update_text(new_q, experiment_id, 0, 0, individual=indiv)
+    q_set.append(new_q)
+    q_exports.append(q_export)
+
+    for i, (before, after) in enumerate(contexts):
+        indiv=False
+        # Generate a discrim turn experiment
+        q_export = experiment_id + f'_Q{i+1}'
+        new_q = q_set_up(q_counter + i + 1, q_export)
+
+        # Fill template with audio choices and text
+        new_q = update_choices(new_q, audio_urls, individual=indiv)
+        new_q = update_text(new_q, experiment_id, before, after, individual=indiv)
+        q_set.append(new_q)
+        q_exports.append(q_export)
+
+    return q_set, q_exports
+
+# make n new blocks according to the survey_length
+def make_blocks(question_ids, basis_blocks):
+    """
+    NOTE this doesn't make new blocks, it just adds questions to the basis_block (elements "Survey Blocks")
+    """
+    new_blocks = basis_blocks
+    block_elements = []
+    for i in range(1,len(question_ids)+1):
+    # for q_id in question_ids:
+        block_element = OrderedDict()
+        block_element['Type'] = 'Question'
+        block_element['QuestionID'] = f'QID{i}'
+        block_elements.append(block_element)
+    new_blocks['Payload'][0]['BlockElements'] = block_elements
+    return new_blocks
+
+# sets the survey ID for any object which needs it
+def set_id(obj):
+    obj['SurveyID'] = config.survey_id
+    return obj
+
+
+
+
+def main():
+    """
+    Given a json file (URLS_PATH in the form {experiment_id: urls}), contruct a qualtrics survey. Order of questions
+    will match the order in the json file. Each question is stored with an export identifier that links it its
+    experiment generation.
+    """
+    # parser = argparse.ArgumentParser() # add question types
+    # parser.add_argument("-ab", action='store_true',
+    #                     help="make A/B questions (like preference test)")
+    # parser.add_argument("-abc", action='store_true',
+    #                     help="make A/B/C questions (like preference test)")
+    # parser.add_argument("-mc", action='store_true',
+    #                     help="make multiple choice questions"
+    #                     "(like error detection)")
+    # parser.add_argument("-trs", action='store_true',
+    #                     help="make transcription questions (with text field)")
+    # parser.add_argument("-mushra", action='store_true',
+    #                     help="make MUSHRA questions with sliders")
+    # parser.add_argument("-mos", action='store_true',
+    #                     help="make Mean Opinion Score questions with sliders")
+    #
+    # args = parser.parse_args()
+    #
+    # # get only args which were specified on command line
+    # args = [key for key, value in vars(args).items() if value==True]
+
+    # get json to use as basis for new questions
+    basis_json = get_basis_json()
+    elements = basis_json['SurveyElements']
+
+    # Set the survey ID in all survey_elements
+    elements = list(map(set_id, elements))
+
+    # get basic survey components from elements JSON
+    basis_blocks = elements[0]
+    basis_flow = elements[1]
+    rs = elements[2]
+    basis_survey_count = elements[7]
+
+    contexts = [(0,0), (2,0), (5,0), (5,5),]
+
+    with open(URLS_PATH) as fs:
+        experiment_ids = json.load(fs)
+
+    # create list to store generated question blocks
+    questions = []
+    question_ids = []
+
+    # create counters to use when indexing optional lists
+    q_counter = 1 # qualtrics question numbering starts at 1
+
+    for exp_id in experiment_ids:
+
+        import IPython
+        IPython.embed()
+
+        new_qs, ids = make_discrim_question_set(q_counter=q_counter,
+                                                experiment_id=exp_id,
+                                                audio_urls=experiment_ids[exp_id],
+                                                contexts=contexts,
+                                                basis_question=elements[12]
+                                                )
+        questions.extend(new_qs)
+        question_ids.extend(ids)
+        q_counter = len(questions)
+
+    # survey_length is determined by number of questions created
+    survey_length = len(questions)
+
+    # Create all the items in survey elements, with helper function where doing so is not trivial
+    blocks = make_blocks(question_ids, basis_blocks)
+    flow = basis_flow
+    flow['Payload']['Properties']['Count'] = survey_length
+    survey_count = basis_survey_count
+    survey_count['SecondaryAttribute'] = str(survey_length)
+    # add all the created elements together
+    elements = [blocks, flow] + elements[2:7]  + questions + [rs]
+
+    # Add the elements to the full survey
+    # Not strictly necessary as we didn't do deep copies of elements
+    out_json = basis_json
+    out_json['SurveyElements'] = elements
+
+    print(f'Generated survey with {survey_length} questions')
+    with open(SAVE_TEMPLATE, 'w+') as outfile:
+        json.dump(out_json, outfile, indent=4)
+
+if __name__ == "__main__":
+    main()
